e53c77b2d473c7f50d333c3b10ae22d0802c007d
K-Jay9 <kjempirecode@gmail.com>
2023-04-15 22:23:57 +0300

    added customtkinter to the project

--- calc/main.py
+++ calc/main.py
@@ -1,8 +1,10 @@
 from tkinter import Tk, Button, Frame, Entry, ttk, PhotoImage, LEFT, Label
 import tkinter.font as font
 
+import customtkinter as ctk
 
 
+ctk.set_appearance_mode("Dark")
 
 
 class calculatorApp:
@@ -35,7 +37,7 @@ class calculatorApp:
     '''
     def hist_button(self):
 
-        self.history_button = Button(self.master, background='black', text="History",fg='white' ,height=2, width=8)
+        self.history_button = ctk.CTkButton(self.master,text="history", corner_radius = 8,height=2, width=8)
 
         self.history_button.place(x=0,y=0)
 
@@ -43,67 +45,74 @@ class calculatorApp:
     Set the display
     '''
     def display(self):
-        self.label = Label(text="",fg='green', bg='gray', height=5, width=23, font=("Helvetica", 18))
-        self.label.place(x=0,y=40)
-        self.update_label()
+        self.label = ctk.CTkEntry(self.master, height=120, width=315, text_color='green', bg_color='white')
 
-    def update_label(self):
-        pass
+        self.label.place(x=0,y=40)
+        
+
+    # def update_label(self, my_text):
+
+    #     self.label.configure(text=my_text)
 
     '''
     The 24 buttons
     '''
     def all_buttons(self):
-        row1 = 0
-        row2 = 0
-        row3 = 0
-        row4 = 0
-        row5 = 0
-        row6 = 0
+        row1 = 2
+        row2 = 2
+        row3 = 2
+        row4 = 2
+        row5 = 2
+        row6 = 2
         y = 165
-        buttons = ["%", "CE", "C", "X", '1/x',"1/x", "x^2", "x^1/2", "/", "7",'8','9','*','4','5','6','-','1','2','3','+','00','0','.','=']
+        buttons = ["%", "CE", "C", "X", '1/x',"1/x", "x^2", "x^1/2", "/", "7",'8','9','*','4','5','6','-','1','2','3','+','00','0','.','last']
 
         # define font
         myFont = font.Font(size=11, weight="bold", family='Courier')
         for i in range(25):
             text = buttons[i]
             if i <= 4:
-                self.btn = Button(self.master, background='#3D3B32', text=text,fg='white' ,height=3, width=11)
+                self.btn = ctk.CTkButton(self.master, text=text,height=50, fg_color='#3D3B32', width=75)
+
                 self.btn.place(x=row1,y=y)
                 row1+= 80
-
                 self.btn['font'] = myFont
 
             elif i > 4 and i <= 8:
-                self.btn = Button(self.master, background='#3D3B32', text=text,fg='white' ,height=3, width=11)
+                self.btn = ctk.CTkButton(self.master, text=text,height=50, fg_color='#3D3B32', width=75)
+
                 self.btn.place(x=row2,y=221)
                 row2+= 80
 
                 self.btn['font'] = myFont
 
             elif i > 8 and i <= 12:
-                self.btn = Button(self.master, background='#3D3B32', text=text,fg='white' ,height=3, width=11)
+                self.btn = ctk.CTkButton(self.master, text=text,height=50, fg_color='#3D3B32', width=75)
+
                 self.btn.place(x=row3,y=277)
                 row3+= 80
 
                 self.btn['font'] = myFont
 
             elif i > 12 and i <= 16:
-                self.btn = Button(self.master, background='#3D3B32', text=text,fg='white' ,height=3, width=11)
+                self.btn = ctk.CTkButton(self.master, text=text,height=50, fg_color='#3D3B32', width=75)
+
                 self.btn.place(x=row4,y=333)
                 row4+= 80
 
                 self.btn['font'] = myFont
 
             elif i > 16 and i <= 20:
-                self.btn = Button(self.master, background='#3D3B32', text=text,fg='white' ,height=3, width=11)
+                self.btn = ctk.CTkButton(self.master, text=text,height=50, fg_color='#3D3B32', width=75)
+
                 self.btn.place(x=row5,y=389)
                 row5+= 80
 
                 self.btn['font'] = myFont
 
             else:
-                self.btn = Button(self.master, background='#3D3B32', text=text,fg='white' ,height=3, width=11)
+                self.btn = ctk.CTkButton(self.master, text=text,height=50, fg_color='#3D3B32', width=75)
+
                 self.btn.place(x=row6,y=445)
                 row6+= 80
 
